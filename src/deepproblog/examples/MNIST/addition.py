ab0638dabd4525662ae58b0981f4349ebe62cc54
Joshua Schroijen <Joshua.Schroijen@gmail.com>
2022-03-18 22:31:07 +0100

    Wrap addition example in function for easy implementation of calibration switch

--- src/deepproblog/examples/MNIST/addition.py
+++ src/deepproblog/examples/MNIST/addition.py
@@ -22,62 +22,66 @@ import torchvision
 import torch.utils.data
 from pathlib import Path
 
-i = int(sys.argv[1]) if len(sys.argv) > 1 else 0
+def main(i=0, calibrate=False):
+  parameters = {
+      "method": ["gm", "exact"],
+      "N": [1, 2, 3],
+      "pretrain": [0],
+      "exploration": [False, True],
+      "run": range(5),
+  }
 
-parameters = {
-    "method": ["gm", "exact"],
-    "N": [1, 2, 3],
-    "pretrain": [0],
-    "exploration": [False, True],
-    "run": range(5),
-}
+  configuration = get_configuration(parameters, i)
+  torch.manual_seed(configuration["run"])
 
-configuration = get_configuration(parameters, i)
-torch.manual_seed(configuration["run"])
+  name = "addition_" + config_to_string(configuration) + "_" + format_time_precise()
+  print(name)
 
-name = "addition_" + config_to_string(configuration) + "_" + format_time_precise()
-print(name)
+  train_set = addition(configuration["N"], "train")
+  test_set = addition(configuration["N"], "test")
 
-train_set = addition(configuration["N"], "train")
-test_set = addition(configuration["N"], "test")
+  network = MNIST_Net()
 
-network = MNIST_Net()
+  pretrain = configuration["pretrain"]
+  if pretrain is not None and pretrain > 0:
+      network.load_state_dict(
+          torch.load("models/pretrained/all_{}.pth".format(configuration["pretrain"]))
+      )
+  net = Network(network, "mnist_net", batching=True)
+  net.optimizer = torch.optim.Adam(network.parameters(), lr=1e-3)
 
-pretrain = configuration["pretrain"]
-if pretrain is not None and pretrain > 0:
-    network.load_state_dict(
-        torch.load("models/pretrained/all_{}.pth".format(configuration["pretrain"]))
-    )
-net = Network(network, "mnist_net", batching=True)
-net.optimizer = torch.optim.Adam(network.parameters(), lr=1e-3)
+  transform_for_calibration = torchvision.transforms.Compose(
+    [torchvision.transforms.ToTensor(), torchvision.transforms.Normalize((0.5,), (0.5,))]
+  )
+  train_set_for_calibration = torchvision.datasets.MNIST(root=str(Path(__file__).parent.joinpath('data')), train=True, download=True, transform=transform_for_calibration)
+  net.calibrate(torch.utils.data.DataLoader(train_set_for_calibration, batch_size=64, shuffle=True))
 
-transform_for_calibration = torchvision.transforms.Compose(
-  [torchvision.transforms.ToTensor(), torchvision.transforms.Normalize((0.5,), (0.5,))]
-)
-train_set_for_calibration = torchvision.datasets.MNIST(root=str(Path(__file__).parent.joinpath('data')), train=True, download=True, transform=transform_for_calibration)
-net.calibrate(torch.utils.data.DataLoader(train_set_for_calibration, batch_size=64, shuffle=True))
-print("NETWORK CALIBRATED NOW!")
+  model = Model("models/addition.pl", [net])
+  if configuration["method"] == "exact":
+      if configuration["exploration"] or configuration["N"] > 2:
+          print("Not supported?")
+          exit()
+      model.set_engine(ExactEngine(model), cache=True)
+  elif configuration["method"] == "gm":
+      model.set_engine(
+          ApproximateEngine(
+              model, 1, geometric_mean, exploration=configuration["exploration"]
+          )
+      )
+  model.add_tensor_source("train", MNIST_train)
+  model.add_tensor_source("test", MNIST_test)
 
-model = Model("models/addition.pl", [net])
-if configuration["method"] == "exact":
-    if configuration["exploration"] or configuration["N"] > 2:
-        print("Not supported?")
-        exit()
-    model.set_engine(ExactEngine(model), cache=True)
-elif configuration["method"] == "gm":
-    model.set_engine(
-        ApproximateEngine(
-            model, 1, geometric_mean, exploration=configuration["exploration"]
-        )
-    )
-model.add_tensor_source("train", MNIST_train)
-model.add_tensor_source("test", MNIST_test)
+  loader = DataLoader(train_set, 2, False)
+  train = train_model(model, loader, 1, log_iter=100, profile=0)
+  model.save_state("snapshot/" + name + ".pth")
+  train.logger.comment(dumps(model.get_hyperparameters()))
+  train.logger.comment(
+      "Accuracy {}".format(get_confusion_matrix(model, test_set, verbose=1).accuracy())
+  )
+  train.logger.write_to_file("log/" + name)
+  return get_confusion_matrix(model, test_set, verbose=1).accuracy()
 
-loader = DataLoader(train_set, 2, False)
-train = train_model(model, loader, 1, log_iter=100, profile=0)
-model.save_state("snapshot/" + name + ".pth")
-train.logger.comment(dumps(model.get_hyperparameters()))
-train.logger.comment(
-    "Accuracy {}".format(get_confusion_matrix(model, test_set, verbose=1).accuracy())
-)
-train.logger.write_to_file("log/" + name)
+if __name__ == '__main__':
+  result_1 = main(1, False)
+  breakpoint()
+  result_2 = main(1, True)
