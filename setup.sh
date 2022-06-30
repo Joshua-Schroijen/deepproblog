50ca2586757d25b7fd74804224386a518c2adc3a
Joshua Schroijen <u0133040@kuleuven.be>
2022-06-30 02:42:19 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 if dpkg -l | grep -q gcc; then
-  sudo apt-get install gcc
+  sudo apt-get install -y gcc
 fi
 if dpkg -l | grep -q swi-prolog; then
-  sudo apt-get install swi-prolog
+  sudo apt-get install -y swi-prolog
 fi
 python3 -m venv DevEnv
 source DevEnv/bin/activate
+pip3 install numpy matplotlib fire
 pip3 install -e .
 pip3 uninstall pyswip pysdd
 pip3 install git+https://github.com/wannesm/PySDD.git#egg=PySDD
