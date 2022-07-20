c0b28188c3e8107cbcdee468cea1676c4feca062
Joshua Schroijen <Joshua.Schroijen@kuleuven.be>
2022-07-20 12:04:29 +0200

    Calibration evaluator now handles multiple confusion matrices in data collection, image download scripts for Coins and Poker

---
+++ src/deepproblog/examples/Poker/data/download_images.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mkdir images
+cd images
+gdown 'https://drive.google.com/uc?id=1awv0RSn8rMVWa6BEQfog79WZbs2OJI2l' -O DeepProbLog_Poker_images.zip
+unzip DeepProbLog_Poker_images.zip
+rm DeepProbLog_Poker_images.zip
