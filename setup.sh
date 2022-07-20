00e44b209e651da964c254553040a44f47ee50c8
Joshua Schroijen <Joshua.Schroijen@kuleuven.be>
2022-07-20 02:26:46 +0200

    Fix wrong PySWIP in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+    #!/bin/bash
 
-sudo apt-get -y update && sudo apt-get -y upgrade
-sudo apt-get install -y python3-pip python3.10-venv gcc swi-prolog libpython3-dev unzip
-python3 -m venv DevEnv
-source DevEnv/bin/activate
-pip3 install numpy matplotlib fire gdown
-pip3 install -e .
-pip3 uninstall -y pyswip pysdd
-pip3 install git+https://github.com/wannesm/PySDD.git#egg=PySDD
-pip3 install git+https://github.com/yuce/pyswip@master#egg=pyswip
+    sudo apt-get -y update && sudo apt-get -y upgrade
+    sudo apt-get install -y python3-pip python3.10-venv gcc swi-prolog libpython3-dev unzip
+    python3 -m venv DevEnv
+    source DevEnv/bin/activate
+    pip3 install fire gdown matplotlib numpy problog torch
+    pip3 install -e .
+    pip3 uninstall -y pyswip pysdd
+    pip3 install git+https://github.com/wannesm/PySDD.git#egg=PySDD
+    pip3 install git+https://github.com/ML-KULeuven/pyswip@development#egg=pyswip
 
-bash src/deepproblog/examples/HWF/data/download_hwf.sh
+    bash src/deepproblog/examples/HWF/data/download_hwf.sh
